af5d2cc399571a6e04d27e794172121433d2e21e
Arya-Chandran <aryachandran86@gmail.com>
2021-11-11 13:21:33 -0500

    updated the router to read the data from json file

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,33 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
+const fs = require('fs');
 
 const api_key = "c6576d2e-b5a7-4a7d-b3b4-435dac3e2e4d";
 
-router.get("/", (req, res) => {
-  axios
-    .get(`https://project-2-api.herokuapp.com/videos?api_key=${api_key}`)
-    .then((response) => {
-      console.log(response.data);
-      res.status(200).json(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json(error);
-    });
+
+router.get('/', (req, res) => {
+  fs.readFile('./data/videos.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(400).json({ message: 'Error getting video data',err });
+    } else {
+      res.status(200).json(JSON.parse(data));
+    }
+  });
 });
 
 router.get('/:id', (req, res) => {
-   const { id } = req.params;
-    axios
-      .get(`https://project-2-api.herokuapp.com/videos/${id}?api_key=${api_key}`)
-      .then((response) => {
-        console.log(response.data);
-        res.status(200).json(response.data);
-      })
-      .catch((error) => {
-        res.status(400).json(error);
-      });
+  console.log(req.params.id);
+  fs.readFile('./data/videos.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.json({ message: 'Error getting video data' });
+    }
+
+    const videoData = JSON.parse(data);
+    const SelectedVideo = videoData.find((video) => video.id == req.params.id);
+    if (!SelectedVideo) {
+      res.json({ message: 'Error getting video data' });
+    } else {
+      res.json(SelectedVideo);
+    }
   });
+});
+
 
 module.exports = router;
+
